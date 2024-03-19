91a9cdc2d72bc67582092b782c22d202685a7fa1
ThibaultGautheron <tgauther@insa-toulouse.fr>
2024-03-19 15:10:30 +0100

    1er push

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,13 +36,16 @@ android {
 }
 
 dependencies {
-
+    implementation("androidx.preference:preference:1.2.1")
+    implementation ("org.osmdroid:osmdroid-android:6.1.14")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
+    implementation(libs.play.services.maps)
+    implementation(libs.play.services.location)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
