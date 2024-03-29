217738654fe4f632b5a4b945ce5b4ee717a94d22
Apollinsa <criquet@insa-toulouse.fr>
2024-03-29 11:44:58 +0100

    de setting vers journey

--- app/src/main/java/com/example/myapplication/UISettings.kt
+++ app/src/main/java/com/example/myapplication/UISettings.kt
@@ -1,21 +1,26 @@
 package com.example.myapplication
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.preference.PreferenceFragmentCompat
+import com.example.myapplication.databinding.ActivitySettingBinding
 
 class UISettings : AppCompatActivity() {
 
+    private lateinit var binding : ActivitySettingBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_setting)
-        if (savedInstanceState == null) {
-            supportFragmentManager
-                .beginTransaction()
-      //          .replace(R.id.settings, SettingsFragment())
-                .commit()
+
+        binding = ActivitySettingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.searchitinerary.setOnClickListener{
+            val intent = Intent(this, JourneyPlanner::class.java)
+            startActivity(intent)
         }
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 
     class SettingsFragment : PreferenceFragmentCompat() {
