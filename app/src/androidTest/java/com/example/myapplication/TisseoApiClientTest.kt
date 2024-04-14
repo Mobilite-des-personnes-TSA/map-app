82d597d181f31dc070fe8ae594618fb34882f209
Jean-Yves <7360784+docjyJ@users.noreply.github.com>
2024-04-14 11:50:11 +0200

    Remove deprecated lib and update code

--- app/src/androidTest/java/com/example/myapplication/TisseoApiClientTest.kt
+++ app/src/androidTest/java/com/example/myapplication/TisseoApiClientTest.kt
@@ -1,15 +1,18 @@
 package com.example.myapplication
 
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.example.myapplication.tisseo.TisseoApiClient
 import org.junit.Assert
 import org.junit.Test
+import org.junit.runner.RunWith
 
+@RunWith(AndroidJUnit4::class)
 class TisseoApiClientTest {
     // Doc Tisséo : https://data.toulouse-metropole.fr/explore/dataset/api-temps-reel-tisseo/files/72b8051dbf567762260cfa66baf8c333/download/
     @Test
     fun testTisseoAutocomplete() {
         val client = TisseoApiClient()
-        val response = client.apiAutocompleteXml("8 allée des sci")
+        val response = client.apiAutocomplete("8 allée des sci")
         Assert.assertNotNull(response)
         println("response :") // Print the response for manual verification
         println(response)
@@ -18,8 +21,7 @@ class TisseoApiClientTest {
     @Test
     fun testTisseoApiJourney() {
         val client = TisseoApiClient()
-        val response = client.apiJourney(
-            "xml", "basso cambo ",
+        val response = client.apiJourney("basso cambo ",
             "françois verdier", "walk", "1"
         )
         Assert.assertNotNull(response)
--- app/src/main/java/com/example/myapplication/tisseo/TisseoApiClient.kt
+++ app/src/main/java/com/example/myapplication/tisseo/TisseoApiClient.kt
@@ -1,86 +1,57 @@
 package com.example.myapplication.tisseo
 
+import android.content.Context
 import android.util.Log
-import org.apache.http.client.HttpClient
-import org.apache.http.client.methods.HttpGet
-import org.apache.http.impl.client.HttpClients
-import org.apache.http.util.EntityUtils
+import java.io.BufferedInputStream
 import java.io.IOException
-import java.io.UnsupportedEncodingException
-import java.net.URLEncoder
+import java.net.URL
+import javax.net.ssl.HttpsURLConnection
+
 
 class TisseoApiClient {
-    private val httpClient  = HttpClients.createDefault()
 
-    @Throws(IOException::class)
-    fun executeGetRequest(service: String, format: String, parameters: String?): String? {
-        val url = buildUrl(service, format, parameters)
-        val httpGet = HttpGet(url)
-        val response = httpClient.execute(httpGet)
-        val entity = response.entity
-        return if (entity != null) {
-            EntityUtils.toString(entity, "UTF-8")
-        } else {
-            null
-        }
-    }
-
-    private fun buildUrl(service: String, format: String, parameters: String?): String {
+    private fun executeGetRequest(service: String, parameters: Map<String, String>): String? {
         val urlBuilder = StringBuilder(ENTRY_POINT)
-        urlBuilder.append(service).append(".").append(format).append("?")
-        if (!parameters.isNullOrEmpty()) {
-            val params =
-                parameters.split("&".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
-            for (param in params) {
-                val keyValue =
-                    param.split("=".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
-                if (keyValue.size == 2) {
-                    try {
-                        urlBuilder.append(URLEncoder.encode(keyValue[0], "UTF-8"))
-                            .append("=")
-                            .append(URLEncoder.encode(keyValue[1], "UTF-8"))
-                            .append("&")
-                    } catch (e: UnsupportedEncodingException) {
-                        e.printStackTrace()
-                    }
-                }
-            }
-        }
-        urlBuilder.append("key=").append(API_KEY)
-        return urlBuilder.toString()
-    }
-
-    fun apiAutocompleteJson(parameter: String): String? {  // return Json with results for autocompletion
-        return apiAutocomplete("json", parameter)
-    }
-
-    fun apiAutocompleteXml(parameter: String): String? {  // return XML with results for autocompletion
-        return apiAutocomplete("xml", parameter)
-    }
-
-    private fun apiAutocomplete(format: String, parameter: String): String? {
-        return try {
-            executeGetRequest("places", format, "term=$parameter")
+            .append(service).append(".json?key=").append(API_KEY)
+        parameters.forEach { (k, v) -> urlBuilder.append("&").append(k).append("=").append(v) }
+        Log.d("TisseoApiClient", "URL: $urlBuilder")
+        val connection = URL(urlBuilder.toString()).openConnection() as HttpsURLConnection
+        val result = try {
+            connection.requestMethod = "GET"
+            connection.connect()
+            BufferedInputStream(connection.inputStream).bufferedReader()
+                .use { it.readText() }
         } catch (e: IOException) {
-            throw RuntimeException(e)
+            null
+        } finally {
+            connection.disconnect()
         }
+        Log.d("TisseoApiClient", "Result: $result")
+        return result
     }
+
+    fun apiAutocomplete(parameter: String) = executeGetRequest(
+        "places", mapOf("term" to parameter)
+    )
 
     fun apiJourney(
-        format: String, departurePlace: String, arrivalPlace: String,
-        roadMode: String, number: String
-    ): String? {
-        return try {
-            executeGetRequest(
-                "journeys", format,
-                "departurePlace=" + departurePlace + "&arrivalPlace=" + arrivalPlace +
-                        "&roadMode=" + roadMode + "&number=" + number +
-                        "&displayWording=1" + "&lang=fr"
-            )
-        } catch (e: IOException) {
-            throw RuntimeException(e)
-        }
-    }
+        departurePlace: String,
+        arrivalPlace: String,
+        roadMode: String,
+        number: String,
+        displayWording: String = "1",
+        lang: String = "fr"
+    ) = executeGetRequest(
+        "journeys",
+        mapOf(
+            "departurePlace" to departurePlace,
+            "arrivalPlace" to arrivalPlace,
+            "roadMode" to roadMode,
+            "number" to number,
+            "displayWording" to displayWording,
+            "lang" to lang
+        )
+    )
 
     companion object {
         private const val ENTRY_POINT = "https://api.tisseo.fr/v2/"
