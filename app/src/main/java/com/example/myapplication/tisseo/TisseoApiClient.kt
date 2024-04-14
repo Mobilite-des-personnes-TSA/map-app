f1dbc93a2afd1dc0c306683437bcb4f3448c9d78
Jean-Yves <7360784+docjyJ@users.noreply.github.com>
2024-04-14 11:04:41 +0200

    Move to kotlin

--- app/src/main/java/com/example/myapplication/tisseo/TisseoApiClient.kt
+++ app/src/main/java/com/example/myapplication/tisseo/TisseoApiClient.kt
@@ -1,90 +1,88 @@
-package com.example.myapplication.tisseo;
+package com.example.myapplication.tisseo
 
-import static com.example.myapplication.tisseo.TisseoKeyKt.API_KEY;
+import android.util.Log
+import org.apache.http.client.HttpClient
+import org.apache.http.client.methods.HttpGet
+import org.apache.http.impl.client.HttpClients
+import org.apache.http.util.EntityUtils
+import java.io.IOException
+import java.io.UnsupportedEncodingException
+import java.net.URLEncoder
 
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.util.EntityUtils;
+class TisseoApiClient {
+    private val httpClient  = HttpClients.createDefault()
 
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
-
-import java.io.IOException;
-
-public class TisseoApiClient {
-
-    private static final String ENTRY_POINT = "https://api.tisseo.fr/v2/";
-
-    private HttpClient httpClient;
-
-    public TisseoApiClient() {
-        this.httpClient = HttpClients.createDefault();
-    }
-
-    public String executeGetRequest(String service, String format, String parameters) throws IOException {
-        String url = buildUrl(service, format, parameters);
-        System.out.println("url :");
-        System.out.println(url);
-        HttpGet httpGet = new HttpGet(url);
-        HttpResponse response = httpClient.execute(httpGet);
-
-        HttpEntity entity = response.getEntity();
-        if (entity != null) {
-            return EntityUtils.toString(entity, "UTF-8");
+    @Throws(IOException::class)
+    fun executeGetRequest(service: String, format: String, parameters: String?): String? {
+        val url = buildUrl(service, format, parameters)
+        val httpGet = HttpGet(url)
+        val response = httpClient.execute(httpGet)
+        val entity = response.entity
+        return if (entity != null) {
+            EntityUtils.toString(entity, "UTF-8")
         } else {
-            return null;
+            null
         }
     }
 
-    private String buildUrl(String service, String format, String parameters) {
-        StringBuilder urlBuilder = new StringBuilder(ENTRY_POINT);
-        urlBuilder.append(service).append(".").append(format).append("?");
-        if (parameters != null && !parameters.isEmpty()) {
-            String[] params = parameters.split("&");
-            for (String param : params) {
-                String[] keyValue = param.split("=");
-                if (keyValue.length == 2) {
+    private fun buildUrl(service: String, format: String, parameters: String?): String {
+        val urlBuilder = StringBuilder(ENTRY_POINT)
+        urlBuilder.append(service).append(".").append(format).append("?")
+        if (!parameters.isNullOrEmpty()) {
+            val params =
+                parameters.split("&".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
+            for (param in params) {
+                val keyValue =
+                    param.split("=".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
+                if (keyValue.size == 2) {
                     try {
                         urlBuilder.append(URLEncoder.encode(keyValue[0], "UTF-8"))
-                                .append("=")
-                                .append(URLEncoder.encode(keyValue[1], "UTF-8"))
-                                .append("&");
-                    } catch (UnsupportedEncodingException e) {
-                        e.printStackTrace();
+                            .append("=")
+                            .append(URLEncoder.encode(keyValue[1], "UTF-8"))
+                            .append("&")
+                    } catch (e: UnsupportedEncodingException) {
+                        e.printStackTrace()
                     }
                 }
             }
         }
-        urlBuilder.append("key=").append(API_KEY);
-        return urlBuilder.toString();
+        urlBuilder.append("key=").append(API_KEY)
+        return urlBuilder.toString()
     }
 
-    public String apiAutocompleteJson(String parameter) {  // return Json with results for autocompletion
-        return apiAutocomplete("json", parameter);
+    fun apiAutocompleteJson(parameter: String): String? {  // return Json with results for autocompletion
+        return apiAutocomplete("json", parameter)
     }
-    public String apiAutocompleteXml(String parameter) {  // return XML with results for autocompletion
-        return apiAutocomplete("xml", parameter);
+
+    fun apiAutocompleteXml(parameter: String): String? {  // return XML with results for autocompletion
+        return apiAutocomplete("xml", parameter)
     }
-    private String apiAutocomplete(String format, String parameter) {
-        try {
-            return this.executeGetRequest("places", format, "term="+parameter);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+
+    private fun apiAutocomplete(format: String, parameter: String): String? {
+        return try {
+            executeGetRequest("places", format, "term=$parameter")
+        } catch (e: IOException) {
+            throw RuntimeException(e)
         }
     }
 
-    public String apiJourney(String format, String departurePlace, String arrivalPlace,
-                             String roadMode, String number) {
-        try {
-            return this.executeGetRequest("journeys", format,
-                    "departurePlace="+departurePlace + "&arrivalPlace="+arrivalPlace +
-                            "&roadMode="+roadMode + "&number="+number +
-                            "&displayWording=1" + "&lang=fr");
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+    fun apiJourney(
+        format: String, departurePlace: String, arrivalPlace: String,
+        roadMode: String, number: String
+    ): String? {
+        return try {
+            executeGetRequest(
+                "journeys", format,
+                "departurePlace=" + departurePlace + "&arrivalPlace=" + arrivalPlace +
+                        "&roadMode=" + roadMode + "&number=" + number +
+                        "&displayWording=1" + "&lang=fr"
+            )
+        } catch (e: IOException) {
+            throw RuntimeException(e)
         }
+    }
+
+    companion object {
+        private const val ENTRY_POINT = "https://api.tisseo.fr/v2/"
     }
 }
--- app/src/test/java/com/example/myapplication/TisseoApiClientTest.kt
+++ app/src/test/java/com/example/myapplication/TisseoApiClientTest.kt
@@ -1,31 +1,29 @@
-package com.example.myapplication;
+package com.example.myapplication
 
-import static org.junit.Assert.*;
+import com.example.myapplication.tisseo.TisseoApiClient
+import org.junit.Assert
+import org.junit.Test
 
-import com.example.myapplication.tisseo.TisseoApiClient;
-
-import org.junit.Test;
-
-public class TisseoApiClientTest {
+class TisseoApiClientTest {
     // Doc Tisséo : https://data.toulouse-metropole.fr/explore/dataset/api-temps-reel-tisseo/files/72b8051dbf567762260cfa66baf8c333/download/
-
     @Test
-    public void testTisseoAutocomplete() {
-        TisseoApiClient client = new TisseoApiClient();
-            String response = client.apiAutocompleteXml("8 allée des sci");
-            assertNotNull(response);
-            System.out.println("response :"); // Print the response for manual verification
-            System.out.println(response);
+    fun testTisseoAutocomplete() {
+        val client = TisseoApiClient()
+        val response = client.apiAutocompleteXml("8 allée des sci")
+        Assert.assertNotNull(response)
+        println("response :") // Print the response for manual verification
+        println(response)
     }
 
     @Test
-    public void testTisseoApiJourney() {
-        TisseoApiClient client = new TisseoApiClient();
-            String response = client.apiJourney("xml", "basso cambo ",
-                    "françois verdier", "walk", "1");
-            assertNotNull(response);
-            System.out.println("response :"); // Print the response for manual verification
-            System.out.println(response);
+    fun testTisseoApiJourney() {
+        val client = TisseoApiClient()
+        val response = client.apiJourney(
+            "xml", "basso cambo ",
+            "françois verdier", "walk", "1"
+        )
+        Assert.assertNotNull(response)
+        println("response :") // Print the response for manual verification
+        println(response)
     }
-
 }
